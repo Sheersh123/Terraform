fc11335400553719b0e3fb3033961323039cf16d
Sheersh <sheershsinha30@gmail.com>
2025-12-26 22:38:44 +0530

    Modified-Terraform-main-file && git-ignore-file

--- Terraform-main/Task-01/main.tf
+++ Terraform-main/Task-01/main.tf
@@ -11,13 +11,14 @@ resource "aws_instance" "east-1" {
     provider = aws
     ami = "ami-068c0051b15cdb816"
     instance_type="t2.micro"
+    subnet_id = "subnet-0a60ca99c5fd94773"
     vpc_security_group_ids = [aws_security_group.demo_group.id]
 }
 resource "aws_instance" "east-2" {
     provider = aws.east-2
     ami = "ami-00e428798e77d38d9"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.demo_group.id]
+    vpc_security_group_ids = [aws_security_group.demo_group_2.id]
 }
 data "aws_vpc" "default" {
     filter {
@@ -28,6 +29,7 @@ data "aws_vpc" "default" {
 
 resource "aws_security_group" "demo_group" {
     name = "demo-group"
+    provider = aws
     description = "Allow HTTP Connection"
     vpc_id = data.aws_vpc.default.id
 
@@ -47,8 +49,9 @@ resource "aws_security_group" "demo_group" {
     }
 }
 resource "aws_security_group" "demo_group_2" {
+    provider =aws.east-2
     description = "allow-HTTP-connection"
-    vpc_id = data.aws_vpc.default
+    vpc_id = data.aws_vpc.default.id
     ingress {
         from_port = "80"
         to_port = "80"
