1f333dee698fd5e94d25ed3cc9c1c2fee040c575
Sheersh <sheershsinha30@gmail.com>
2026-01-12 22:18:31 +0530

    Modified-example-main-file

--- Terraform-main/modules/s3/example-main.tf
+++ Terraform-main/modules/s3/example-main.tf
@@ -1,11 +1,11 @@
 ### Create  three S3 buckets using the count meta-argument.
 ### Each bucket should include the index number to its name.(For Example Student-demo-bucket-1,Student-demo-bucket -2)
 
-resource "aws_s3_bucket" "Student-demo-bucket" {
+resource "aws_s3_bucket" "student-demo-bucket-demo" {
     count = 3
-    bucket = "Student-demo-bucket-${count.index + 1}"
-    acl = "private"
+    bucket = "student-demo-bucket-demo-${count.index + 1}"
+    acl  = "private"
     tags = {
-        Name = "Student-demo-bucket-${count.index + 1}"
+        Name = "student-demo-bucket-demo-${count.index + 1}"
     }
 }
